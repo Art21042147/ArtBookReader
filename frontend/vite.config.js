2950cfaae2bef18047091ad7f78800f6be9fd719
Art21042147 <art21042147@gmail.com>
2025-04-11 18:09:17 +0300

    fix vite path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/static/',
   build: {
     outDir: '../static',
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
       output: {
         assetFileNames: 'assets/[name].[hash][extname]',
         chunkFileNames: 'assets/[name].[hash].js',
